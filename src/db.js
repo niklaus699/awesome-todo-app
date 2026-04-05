2ed45186f06091240ecdacc49e2c4641fdd38f12
niklaus699 <nicholasbasseyy@gmail.com>
2026-04-05 03:02:02 +0100

    used a dynamic conditional statement to set ssl connections

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const isProduction = process.env.DATABASE_URL?.includes("render.com");
+const isProduction = process.env.NODE_ENV === "production";
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: isProduction ? {
